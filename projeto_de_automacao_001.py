26e90c8964723db6c1df9f5c1d4e3252ce8c149c
Carlos Henrique Barros Silva Campos <63325025+CarlosH2020@users.noreply.github.com>
2021-05-16 12:36:01 -0300

    Add files via upload

---
+++ projeto_de_automacao_001.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pyautogui as py
+import time
+
+# Url que vai ser usada...
+url = 'https://www.mercadobitcoin.com.br/'
+driver = webdriver.Chrome()
+
+
+def chamada_driver():
+    driver.get(url)
+    # Usando o Xpath para extrair valores..
+    btc = driver.find_element_by_xpath('/html/body/section[1]/div/div[2]/div/div/div[2]/div[2]').text
+    xrp = driver.find_element_by_xpath('/html/body/section[1]/div/div[2]/div/div/div[3]/div[1]/div/div[2]').text
+    moedas = [btc, xrp]
+    for moeda in moedas:
+        print('=' * 46)
+        print(moeda)
+        print('=' * 46)
+
+
+if __name__ == '__main__':
+    py.alert(text='Bora começar?', title='Analise de criptomoedas', button='OK')
+    chamada_driver()
+    time.sleep(3)
+    py.click(x=1027, y=19)
+
+
+
+
+
+
+
+
